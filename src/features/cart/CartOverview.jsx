2351f3d7226071764be1f9461e4f7b7fd85d7db8
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-10-04 20:58:16 +0600

    add tailwind styles

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,9 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="bg-stone-800 px-4 py-4 uppercase flex justify-between items-center text-stone-200 sm:px-6 ">
+      <p className="space-x-4 text-sm font-semibold text-stone-100 sm:space-x-6 md:text-base">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,18 +1,20 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOverview';
-import Header from './Header';
-import Loader from './Loader';
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
   const nagigation = useNavigation();
-  const isLoading = nagigation.state === 'loading';
+  const isLoading = nagigation.state === "loading";
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto  max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
