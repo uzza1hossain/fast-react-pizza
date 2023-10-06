931199115132c69595f3bd674a53bfa0c9d9729c
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-10-06 11:07:16 +0600

    add eslint perfectionist plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,56 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "perfectionist"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
+    ],
+    "perfectionist/sort-imports": [
+      "warn",
+      {
+        type: "alphabetical",
+        order: "asc",
+        groups: [
+          "type",
+          "react",
+          "nanostores",
+          ["builtin", "external"],
+          "internal-type",
+          "internal",
+          ["parent-type", "sibling-type", "index-type"],
+          ["parent", "sibling", "index"],
+          "side-effect",
+          "style",
+          "object",
+          "unknown",
+        ],
+        "custom-groups": {
+          value: {
+            react: ["react", "react-*"],
+            nanostores: "@nanostores/**",
+          },
+          type: {
+            react: "react",
+          },
+        },
+        "newlines-between": "always",
+        "internal-pattern": [
+          "src/ui/**",
+          "src/feature/**",
+          "src/services/**",
+          "src/utils/**",
+        ],
+      },
     ],
   },
 };
