25090ca7dd2e119e2c235486366f13b809668f0f
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-10-06 21:08:05 +0600

    add selector function getTotalCartQuantity & getTotalCartPrice

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,18 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  console.log(totalCartQuantity);
   return (
-    <div className="bg-stone-800 px-4 py-4 uppercase flex justify-between items-center text-stone-200 sm:px-6 ">
+    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 uppercase text-stone-200 sm:px-6 ">
       <p className="space-x-4 text-sm font-semibold text-stone-100 sm:space-x-6 md:text-base">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>${totalCartPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,7 +1,20 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { addItem } from "../cart/cartSlice";
+import { useDispatch } from "react-redux";
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name: name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -24,7 +37,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
