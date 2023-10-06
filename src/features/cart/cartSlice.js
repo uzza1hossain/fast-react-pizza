9902da07e025d3223526fd1ff5567cb27dd8362f
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-10-06 23:40:25 +0600

    add UpdateItemQuantity

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -23,6 +23,7 @@ const cartSlice = createSlice({
       const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -44,7 +45,7 @@ export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
 export const getTotalCartPrice = (state) =>
-  state.cart.cart.reduce((sum, item) => sum + item.unitPrice, 0);
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 export const getCurrentQuantityById = (id) => (state) =>
   state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,6 +6,7 @@ function Button({ children, disabled, to, type, onClick }) {
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
+    round: base + " px2.5 py-1 md:px-3.5 md:py-2 text-sm rounded-full",
     small: base + " px-4 py-3 md:px-5 md:py-2.5 text-xs",
     secondary:
       "text-sm inline-block rounded-full border-2 border-stone-300  font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring-2 focus:ring-stone-200 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5 hover:text-stone-800 focus:text-stone-800",
