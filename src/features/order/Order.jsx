12b4ef9d98fa1410fe1c293e8a5859f70774aacc
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-10-07 23:29:21 +0600

    add action for UpdateOrder using useFetcher

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
+import UpdateOrder from "./UpdateOrder";
 function Order() {
   const order = useLoaderData();
   const fetcher = useFetcher();
@@ -88,6 +89,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
